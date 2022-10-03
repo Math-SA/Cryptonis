72559c41e2241f13a5cc42718e55583503db2ee0
Matheus Assis <matheus.s.assis@gmail.com>
2022-10-03 15:52:13 -0300

    fix: currency adhere to rfc8259

--- app/Model/CoinInfo.ts
+++ app/Model/CoinInfo.ts
@@ -16,7 +16,7 @@ export default class CoinInfo {
     public toJSON(){
         let prices = new Array<string>();
         this.prices.forEach((price)=>{
-            prices.push(`${price.currency}:${price.value}`);
+            prices.push(`"${price.currency}":${price.value}`);
         });
         return `{"coin":"${this.coin.id}", "current_price":{${prices.join(',')}}, "last_updated":${this.updatedAt} }`;
     }
